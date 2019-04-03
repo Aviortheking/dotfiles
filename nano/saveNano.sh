f96a86c423125bc8cd171dc62922cf1c63d95f6e
Avior <florian.bouillon@delta-wings.net>
2019-04-03 10:40:37 +0200

    Made nano config

---
+++ nano/saveNano.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+C_WHITE="\033[38;5;15m"
+NO_FORMAT="\033[0m"
+C_BLUE="\033[38;5;12m"
+C_GREEN="\033[38;5;2m"
+
+path="$(dirname $0)"
+
+echo -en "\rSaving nano config [${C_BLUE}¤${NO_FORMAT}]"
+
+###
+# save config
+###
+cp ~/.nanorc $path/
+
+echo -en "\rSaving nano config [${C_GREEN}+${NO_FORMAT}]\n"
