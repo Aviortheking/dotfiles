af64ab6ec5f603061f51194510ad2fec9a1de64f
Avior <florian.bouillon@delta-wings.net>
2019-04-03 21:10:30 +0200

    Updated omf

---
+++ omf/omf.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+C_WHITE="\033[38;5;15m"
+NO_FORMAT="\033[0m"
+C_BLUE="\033[38;5;12m"
+C_GREEN="\033[38;5;2m"
+
+path="$(dirname $0)"
+
+#check if Fish exist
+which fish > /dev/null 2>&1
+if [ ! $? -eq 0 ]; then
+	echo "Fish not found"
+fi
+#check if omf exist
+fish -c 'omf' > /dev/null 2>&1
+if [ ! $? -eq 0 ]; then
+	echo "OMF not found"
+fi
+
+packagesFile="packages.txt"
+
+###
+# save config
+###
+function save {
+	echo -en "\rSaving Oh My Fish addons [${C_BLUE}¤${NO_FORMAT}]"
+
+	fish -c "omf list | tr '\t' '\n' | grep '^[a-z-]'> ${path}/$packagesFile"
+
+	echo -en "\rSaving Oh My Fish addons [${C_GREEN}+${NO_FORMAT}]\n"
+}
+
+###
+# save config
+###
+function load {
+	echo -e "Loading Oh My Fish addons [${C_BLUE}¤${NO_FORMAT}]"
+
+	cat $path/$packagesFile | while read line
+	do
+		fish -c "omf install ${line}"
+	done
+	fish -c "omf theme"
+	read -p "Select the theme to use: " theme
+	fish -c "omf theme $theme"
+	fish -c "omf reload" # if user is in fish
+
+	echo -e "Loading Oh My Fish addons [${C_GREEN}+${NO_FORMAT}]"
+}
+
+function install {
+	curl -L https://get.oh-my.fish | fish
+}
+
+if [ -z $* ];then
+	read -p "save, load or install? " e
+	$e
+else
+	$*
+fi
