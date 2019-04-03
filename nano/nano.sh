5580c3796fe89994fd8de9b33200a0b06cc6da59
Avior <florian.bouillon@delta-wings.net>
2019-04-03 18:57:04 +0200

    Updated nano scripts

---
+++ nano/nano.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+C_WHITE="\033[38;5;15m"
+NO_FORMAT="\033[0m"
+C_BLUE="\033[38;5;12m"
+C_GREEN="\033[38;5;2m"
+
+path="$(dirname $0)"
+
+###
+# save config
+###
+function save {
+	echo -en "\rSaving nano config [${C_BLUE}¤${NO_FORMAT}]"
+
+	cp ~/.nanorc $path/
+
+	echo -en "\rSaving nano config [${C_GREEN}+${NO_FORMAT}]\n"
+}
+
+###
+# save config
+###
+function load {
+	echo -en "\rLoading nano config [${C_BLUE}¤${NO_FORMAT}]"
+
+	cp $path/.nanorc ~/
+
+	echo -en "\rLoading nano config [${C_GREEN}+${NO_FORMAT}]\n"
+}
+
+function install {
+	$path/../function.sh install nano
+}
+
+if [ -z $* ];then
+	read -p "save, load or install? " e
+	$e
+else
+	$*
+fi
