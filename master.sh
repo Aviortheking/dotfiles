345b53626801cfdde7bb91f367b1bcdda90d3b06
Avior <florian.bouillon@delta-wings.net>
2019-05-06 11:23:22 +0200

    Changed master.sh to make it work

--- master.sh
+++ master.sh
@@ -1,31 +1,26 @@
 #!/bin/bash
 
-###
-#	WIP function
-#	I need to remake all config before safely using it
-###
-echo "This is a WIP script please launch one by one the scripts you want ot backup/load"
-exit 1
-
 scripts=(
-	"VSCode/vscode.sh"
-	"omf/omf.sh"
-	"nano/nano.sh"
-	"hyperJS/hyperjs.sh"
-	"fish/fish.sh"
+	"vscode"
+	"omf"
+	"nano"
+	"hyperjs"
+	"fish"
 )
 
 function save {
 	# save
 	for item in ${scripts[*]}
 	do
-		$item save
+		$item/$item.sh save
 	done
 }
 
 function load {
-	#check if installed if not ask
-	#load config
+	for item in ${scripts[*]}
+	do
+		$item/$item.sh load
+	done
 }
 
 
